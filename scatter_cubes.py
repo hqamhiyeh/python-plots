75733a1a6bd6e5f05791400312a7183f3013935a
Haitham Qamhiyeh <git@qamhiyeh.dev>
2025-08-03 19:02:11 -0500

    [cubes] Plot cube values with scatter

---
+++ scatter_cubes.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+x_values = range(1,5001)
+y_values = [x**3 for x in x_values]
+
+fig, ax = plt.subplots(figsize=(16,10))
+ax.scatter(x_values, y_values, s=10)
+
+# Set chart title and label axes.
+ax.set_title("Cube Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Cube of Value", fontsize=14)
+
+# Set the range for each axis.
+ax.axis([0, 5100, 0, 130_000_000_000])
+ax.ticklabel_format()
+
+# Set size of tick labels.
+ax.tick_params(labelsize=14)
+
+plt.show()
